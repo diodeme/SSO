cfe9c2228529c78c83a80b371ce5090bd1a8622d
diode <diodemeu@gmail.com>
2019-05-15 10:42:10 +0800

    version 1.5

---
+++ src/main/java/com/thunisoft/web/config/MyWebAppConfigurer.java
@@ -0,0 +1,19 @@
+package com.thunisoft.web.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * @Author: Diodeme
+ * @Date: 2019/5/15 10:06
+ * @Version 1.0
+ */
+@Configuration
+public class MyWebAppConfigurer extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
+}
