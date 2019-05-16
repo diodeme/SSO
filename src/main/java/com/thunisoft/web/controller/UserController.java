b8da7681134e35f9e8e090f3edbb0919215a9739
diode <diodemeu@gmail.com>
2019-05-16 09:33:46 +0800

    version 2.0 Before jpa->mybatis

--- src/main/java/com/thunisoft/web/controller/UserController.java
+++ src/main/java/com/thunisoft/web/controller/UserController.java
@@ -1,28 +1,70 @@
 package com.thunisoft.web.controller;
 
-import com.thunisoft.web.mapper.UserMapper;
-import com.thunisoft.web.model.User;
+import com.thunisoft.web.model.webResult;
+import com.thunisoft.web.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- * @Author: Diodeme
- * @Date: 2019/5/14 13:15
- * @Version 1.0
- */
-@RestController
-@RequestMapping
-public class UserController {
-    @Autowired
-    UserMapper userMapper;
-    @GetMapping("/user")
-    public String getUser(@RequestParam int id)
-    {
-        User user=userMapper.getUserById(id);
-        return user.toString();
-    }
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+@RestController
+@RequestMapping("/user")
+public class UserController {
+	/**
+     * 注入userService
+	 */
+	@Autowired
+	private UserService userService;
+
+	/**
+	 * 用户登录controller，直接与前端进行交互的函数
+	 * @param username 用户名
+	 * @param password 密码
+	 * @param request 似乎是自动生成
+	 * @param response 似乎是自动生成
+	 * @return ItdragonResult对象
+	 */
+	@RequestMapping(value="/login", method= RequestMethod.POST)
+	public webResult userLogin(String username, String password,
+                               HttpServletRequest request, HttpServletResponse response) {
+		try {
+			webResult result = userService.userLogin(username, password, request, response);
+			return result;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return webResult.build(500, "");
+		}
+	}
+
+    /**
+     * 注销
+	 * @param token 用户token
+	 * @return index页面
+	 */
+	@RequestMapping(value="/logout/{token}")
+	public String logout(@PathVariable String token) {
+		userService.logout(token); // 思路是从Redis中删除key，实际情况请和业务逻辑结合
+		return "index";
+	}
+
+    /**
+     * 根据token寻找用户
+	 * @param token 用户token
+	 * @return ItdragonResult对象
+	 */
+	@RequestMapping("/token/{token}")
+	public Object getUserByToken(@PathVariable String token) {
+		webResult result = null;
+		try {
+			result = userService.queryUserByToken(token);
+		} catch (Exception e) {
+			e.printStackTrace();
+			result = webResult.build(500, "");
+		}
+		return result;
+	}
 }
---
+++ src/main/java/com/thunisoft/web/controller/UserController_bak.java
@@ -0,0 +1,28 @@
+package com.thunisoft.web.controller;
+
+import com.thunisoft.web.mapper.UserMapper;
+import com.thunisoft.web.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author: Diodeme
+ * @Date: 2019/5/14 13:15
+ * @Version 1.0
+ */
+@RestController
+@RequestMapping
+public class UserController_bak {
+    @Autowired
+    UserMapper userMapper;
+    @GetMapping("/user")
+    public String getUser(@RequestParam int id)
+    {
+        User user=userMapper.getUserById(id);
+        return user.toString();
+    }
+
+}
