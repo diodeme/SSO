90d198fd217a333001d9f41314cda7fdc54a5139
diode <diodemeu@gmail.com>
2019-05-16 10:29:49 +0800

    version 2.2.2

--- src/main/java/com/thunisoft/web/model/User.java
+++ src/main/java/com/thunisoft/web/model/User.java
@@ -19,11 +19,7 @@ public class User {
     private String plainPassword; 			// 登录时的密码，不持久化到数据库
     private String password;				// 加密后的密码
     private String salt;					// 用于加密的盐
-    private String iphone;					// 手机号
-    private String email;					// 邮箱
-    private String platform;				// 用户来自的平台
-    private String createdDate;				// 用户注册时间
-    private String updatedDate;				// 用户最后一次登录时间
+
 
     public Long getId() {
         return id;
@@ -61,40 +57,10 @@ public class User {
     public void setSalt(String salt) {
         this.salt = salt;
     }
-    public String getIphone() {
-        return iphone;
-    }
-    public void setIphone(String iphone) {
-        this.iphone = iphone;
-    }
-    public String getEmail() {
-        return email;
-    }
-    public void setEmail(String email) {
-        this.email = email;
-    }
-    public String getPlatform() {
-        return platform;
-    }
-    public void setPlatform(String platform) {
-        this.platform = platform;
-    }
-    public String getCreatedDate() {
-        return createdDate;
-    }
-    public void setCreatedDate(String createdDate) {
-        this.createdDate = createdDate;
-    }
-    public String getUpdatedDate() {
-        return updatedDate;
-    }
-    public void setUpdatedDate(String updatedDate) {
-        this.updatedDate = updatedDate;
-    }
+
     @Override
     public String toString() {
         return "User [id=" + id + ", account=" + account + ", userName=" + userName + ", plainPassword=" + plainPassword
-                + ", password=" + password + ", salt=" + salt + ", iphone=" + iphone + ", email=" + email
-                + ", platform=" + platform + ", createdDate=" + createdDate + ", updatedDate=" + updatedDate + "]";
+                + ", password=" + password + ", salt=" + salt +"]";
     }
 }
---
+++ src/main/java/com/thunisoft/web/service/UserService.java
@@ -0,0 +1,131 @@
+package com.thunisoft.web.service;
+
+import com.thunisoft.web.model.User;
+import com.thunisoft.web.model.webResult;
+import com.thunisoft.web.repository.JedisClient;
+import com.thunisoft.web.dao.UserDao;
+import com.thunisoft.web.utils.CookieUtils;
+import com.thunisoft.web.utils.JsonUtils;
+import com.thunisoft.web.utils.webUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.transaction.Transactional;
+import java.util.UUID;
+
+/**
+ *
+ * 它负责三件事:
+ * 第一件事情：验证用户信息是否正确，并将登录成功的用户信息保存到Redis数据库中。
+ * 第二件事情：负责判断用户令牌是否过期，若没有则刷新令牌存活时间。
+ * 第三件事情：负责从Redis数据库中删除用户信息。
+ * 主要有四个函数：
+ * registerUser：用户注册
+ * userLogin：用户登录
+ * logout：用户注销
+ * queryUserByToken：根据用户token在redis查找用户信息
+ */
+@Service
+//@Transactional
+@PropertySource(value = "classpath:redis.properties")
+public class UserService {
+	
+	@Autowired
+	private UserDao userDao;
+	
+	@Autowired
+	private JedisClient jedisClient;
+	
+	@Value("${REDIS_USER_SESSION_KEY}")
+	private String REDIS_USER_SESSION_KEY;
+	
+	@Value("${SSO_SESSION_EXPIRE}")
+	private Integer SSO_SESSION_EXPIRE;
+
+    /**
+     * 用户注册
+     * @param user 用户对象
+     * @return ItdragonResult对象
+     */
+    public webResult registerUser(User user) {
+    	// 检查用户名是否注册，一般在前端验证的时候处理，因为注册不存在高并发的情况，这里再加一层查询是不影响性能的
+    	if (null != userDao.findByAccount(user.getAccount())) {
+    	    //如果已经注册，返回报错
+    		return webResult.build(400, "");
+    	}
+    	userDao.save(user);
+    	// 注册成功后选择发送邮件激活。现在一般都是短信验证码
+    	return webResult.build(200, "");
+    }
+
+    /**
+     *
+     * @param account 账户
+     * @param password 密码
+     * @param request 客户端请求
+     * @param response 服务器响应
+     * @return ItdragonResult对象
+     */
+    public webResult userLogin(String account, String password,
+                               HttpServletRequest request, HttpServletResponse response) {
+    	// 判断账号密码是否正确
+		User user = userDao.findByAccount(account);
+		//ItdragonUtils用于加密解密
+		if (!webUtils.decryptPassword(user, password)) {
+			return webResult.build(400, "账号名或密码错误");
+		}
+		// 生成token
+		String token = UUID.randomUUID().toString();
+		// 清空密码和盐避免泄漏
+		String userPassword = user.getPassword();
+		String userSalt = user.getSalt();
+		user.setPassword(null);
+		user.setSalt(null);
+		// 把用户信息写入 redis
+		jedisClient.set(REDIS_USER_SESSION_KEY + ":" + token, JsonUtils.objectToJson(user));
+		// user 已经是持久化对象了，被保存在了session缓存当中，
+        // 若user又重新修改了属性值，那么在提交事务时，
+        // 此时 hibernate对象就会拿当前这个user对象和保存在session缓存中的user对象进行比较，
+        // 如果两个对象相同，则不会发送update语句，否则，如果两个对象不同，则会发出update语句。
+		user.setPassword(userPassword);
+		user.setSalt(userSalt);
+		// 设置 session 的过期时间 expire方法用于设置过期时间 SSO_SESSION_EXPIRE在配置文件中进行配置
+		jedisClient.expire(REDIS_USER_SESSION_KEY + ":" + token, SSO_SESSION_EXPIRE);
+		// 添加写 cookie 的逻辑，cookie 的有效期是关闭浏览器就失效。
+		CookieUtils.setCookie(request, response, "USER_TOKEN", token);
+		// 返回token
+		return webResult.ok(token);
+	}
+
+    /**
+     * 注销删除token
+	 * @param token 用户token
+	 */
+	public void logout(String token) {
+    	jedisClient.del(REDIS_USER_SESSION_KEY + ":" + token);
+    }
+
+    /**
+     * 根据token从redis中查询用户信息
+     * @param token
+     * @return ItdragonResult对象
+     */
+	public webResult queryUserByToken(String token) {
+		// 根据token从redis中查询用户信息
+		String json = jedisClient.get(REDIS_USER_SESSION_KEY + ":" + token);
+		// 判断是否为空
+		if (StringUtils.isEmpty(json)) {
+			return webResult.build(400, "此session已经过期，请重新登录");
+		}
+		// 更新过期时间
+		jedisClient.expire(REDIS_USER_SESSION_KEY + ":" + token, SSO_SESSION_EXPIRE);
+		// 返回用户信息
+		return webResult.ok(JsonUtils.jsonToPojo(json, User.class));
+	}
+    
+}
